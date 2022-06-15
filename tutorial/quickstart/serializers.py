06b5bfe4e4579e8a8967feb2ac359ca3e1ab17b7
Saddam Husein <saddambinhusein2@gmail.com>
2022-06-15 16:07:52 +0800

    tambahan: revisi field 'is_staff' menjadi 'groups' pada UserSerializer

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['url', 'username', 'email', 'groups']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
