020d7ec50cc0caa5a3e6881c91f82e42473c3db9
Saddam Husein <saddambinhusein2@gmail.com>
2022-06-20 21:21:42 +0800

    mengubah kode dari tutorial 1 dengan decorator @api_view

---
+++ README.md
@@ -0,0 +1,7 @@
+# djangorestID
+
+Mastering Django Rest Framework Tutorial Indonesia
+
+## Django REST Framework Tutorial 2: Requests and Responses
+
+
---
+++ tutorial/snippets/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+# Create your views here.
+
+@api_view(['GET', 'POST'])
+def snippet_list(request):
+    if request.method == 'GET':
+        snippet = Snippet.objects.all()
+        serializer = SnippetSerializer(snippet, many=True)  # many: there are many objects in our models
+        return Response(serializer.data)  # without the status code means HTTP_200_OK. we can also write the identifier status code using by numeric identifier (e.g. status=200). but it's good idea to use  explicit identifiers (e.g. status=status.HTTP_200_OK) than using numeric identifiers. 
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
