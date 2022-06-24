6ee30648b7669f4fc68dedaa6e880eaf23f82fe5
Saddam Husein <saddambinhusein2@gmail.com>
2022-06-25 01:46:05 +0800

    coding DRF Mixins

--- README.md
+++ README.md
@@ -19,3 +19,19 @@ Mastering Django Rest Framework Tutorial Indonesia
 * Testing POST request with curl:\
 ```curl -v -d code='print(123)' http://127.0.0.1:8000/snippets/``` # Content-Type: application x-www-form-urlencoded\
 ```curl -v -X POST -d "{\"code\": \"print(456)\"}" -H "Content-Type: application/json" http://127.0.0.1:8000/snippets/``` # Content-Type: application/json
+
+**Resources:**
+* About APIView:\
+https://www.django-rest-framework.org/api-guide/views/#class-based-views
+
+* About Django CBV mixins:\ 
+https://docs.djangoproject.com/en/4.0/topics/class-based-views/mixins/
+
+* About DRF mixins:\
+https://www.django-rest-framework.org/api-guide/generic-views/#mixins
+
+* About GenericAPIView:\
+https://www.django-rest-framework.org/api-guide/generic-views/#genericapiview
+
+**Refactoring of Our API views:**\
+FBV (@api_view decorator) --> CBV (APIView class)  --> CBV (using DRF mixins)
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,51 +1,43 @@
-from django.http import Http404
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
 # Create your views here.
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):  # GenericAPIView extends Rest framework's APIView
+    queryset = Snippet.objects.all()  # GenericAPIView attributes
+    serializer_class = SnippetSerializer  # GenericAPIView attributes
 
-class SnippetList(APIView):
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+    '''
+        by adding queryset and serializer_class above, now the .get()
+        and .post() methods that we are binding below, already have
+        queryset (returning objects) and serializer (serializing output
+        and deserializing input)
+    '''
 
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):  # explicitly binding the get() from APIView
+        return self.list(request, *args, **kwargs)  # .list(): ListModelMixin method
+
+    def post(self, request, *args, **kwargs):  # explicitly binding the post() from APIView
+        return self.create(request, *args, **kwargs)  # .create(): CreateModelMixin method
 
 
-class SnippetDetail(APIView):
-    def get_object(self, pk):  # we create helping method to get single object
-        try:
-            snippet = Snippet.objects.get(pk=pk)
-            return snippet
-        except Snippet.DoesNotExist:
-            raise Http404
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)  # .retrieve(): RetrieveModelMixin method
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):  # explicitly binding the put() from APIView
+        return self.update(request, *args, **kwargs)  # .update(): UpdateModelMixin method
 
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):  # explicitly binding the delete() from APIView
+        return self.destroy(request, *args, **kwargs)  # .retrieve(): DestroyModelMixin method
