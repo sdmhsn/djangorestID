82ed02675f3e8f69e9f27999789e41ec8775083e
Saddam Husein <saddambinhusein2@gmail.com>
2022-07-01 00:23:51 +0800

    - memastikan URL patterns menggunakan nama - modifikasi menggabungkan urlpatterns dengan format_suffix_patterns

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,16 +2,14 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     path('', views.api_root),  # fbv
     # path('', views.ApiRoot.as_view()),  # cbv
 
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 
     path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+])
